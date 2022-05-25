3bd5ee528c3fae217e4ae22eb07671e6f744d04c
ketkikk <kulketki7@gmail.com>
2022-05-25 12:17:03 +0530

    first commit

---
+++ arithmatical operators/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Class1
+    {
+      
+     for (int row = 1; row <= 5; row++) ;
+            {
+                for (int col= 1; col <= col; col++);
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
